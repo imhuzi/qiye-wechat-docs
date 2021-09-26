8e84342f912b6e3936edeebb7334a685e462628d
Huzi.Wang <huzi.wh@gmail.com>
2021-09-26 17:33:30 +0800

    完善配置“

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd .vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.qiye.chat' > CNAME
+
+git init
+git add .
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git remote add origin git@github.com:imhuzi/qiye-wechat-docs.git
+git branch -M main
+git push -f git@github.com:imhuzi/qiye-wechat-docs.git main:gh-pages
+
+cd -
